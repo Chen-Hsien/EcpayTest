188fc71fded41cdc99a26fbf486ab63ac4c49096
Chen-Hsien <zxcvb1088@gmail.com>
2024-02-19 18:14:10 +0800

    go

---
+++ src/app/api/delivery/address/family_mart_callback/route.ts
@@ -0,0 +1,11 @@
+import { redirect } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const body = await req.json();
+
+  console.log(body, "<< body 🐧🐧🐧");
+
+  // redirect to the frontend main page with relative path
+  return redirect("../..");
+}
